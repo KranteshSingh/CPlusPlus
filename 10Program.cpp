8af9a50517a397b45d9b72711af6c21922db109e
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-09-28 18:57:29 +0530

    C++ Program

--- 10Program.cpp
+++ 10Program.cpp
@@ -1,23 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int min(int x, int y)
+{
+    return (x < y) ? x : y;
+}
+
+int calcAngle(double h, double m)
+{
+
+    if (h < 0 || m < 0 || h > 12 || m > 60)
+        printf("Wrong input");
+
+    if (h == 12)
+        h = 0;
+    if (m == 60)
+        m = 0;
+
+    int hour_angle = 0.5 * (h * 60 + m);
+    int minute_angle = 6 * m;
+
+    int angle = abs(hour_angle - minute_angle);
+
+    angle = min(360 - angle, angle);
+
+    return angle;
+}
+
 int main()
 {
     int t;
     cin >> t;
     while (t != 0)
     {
-        int hour, minute;
-        float hmin, hAngle, mAngle;
+
+        int hour, minute, fres;
         cin >> hour >> minute;
-        hmin = (float)minute / 60;
-        hAngle = (hour + hmin) * 30;
-        mAngle = hmin * 300;
-
-        cout << hAngle - mAngle << endl;
-
+        fres = calcAngle(hour, minute);
+        cout << fres << endl;
         t--;
     }
-
     return 0;
 }
